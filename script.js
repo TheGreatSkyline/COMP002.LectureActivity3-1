6d1e6afdab569fccf515ed2d47a88308fc9fb9b4
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-10-30 23:41:47 -0700

    Made function that gets the circumference of a circle

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function myGreet(name) {
   }//the function myGreet ask for the name and returns "Hello, name!"
 
 console.log(myGreet("Joaquin"));
+
+// Create a function called getCircumference that returns the circumference of a circle
+function getCircumference(diameter) {
+  const pi = 3.14159;
+  return pi * diameter;
+}
+//the function getCircumference ask for the diameter and returns the circumference
+console.log(getCircumference(10));  // Should output approximately 31.4159
