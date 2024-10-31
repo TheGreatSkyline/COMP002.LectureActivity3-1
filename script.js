3eecd6830299c1c2d89f93f50de08a28addc5963
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-10-30 23:19:27 -0700

    Create a Function called myGreet

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create a function called myGreet
+function myGreet(name) {
+    return "Hello, " + name + "!";
+  }//the function myGreet ask for the name and returns "Hello, name!"
+
+console.log(myGreet("Joaquin"));
